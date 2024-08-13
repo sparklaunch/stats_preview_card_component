cf20bc47cd695d1c5d445085cc22444d9e9fc4a6
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-08-13 13:25:08 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# stats_preview_card_component
+ 
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:stats_preview_card_component/screens/main_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+    MaterialApp(
+      theme: _buildTheme(Brightness.dark),
+      home: const Scaffold(
+        body: MainScreen(),
+      ),
+    ),
+  );
+}
+
+ThemeData _buildTheme(Brightness brightness) {
+  final ThemeData baseTheme = ThemeData(brightness: brightness);
+  return baseTheme.copyWith(
+      textTheme: GoogleFonts.interTextTheme(baseTheme.textTheme));
+}
